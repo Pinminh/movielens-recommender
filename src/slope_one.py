cfcf57eb0cc1afc0da5cadf4a2d17872fd863b2c
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-10-18 23:09:24 +0700

    feat(slope): revise console messages

--- src/slope_one.py
+++ src/slope_one.py
@@ -17,31 +17,32 @@ full_testset = full_trainset.build_testset()
 trainset, testset = train_test_split(data, test_size=0.25)
 predictions = algo.fit(trainset).test(testset)
 
-print("Cross validation of 10 folds for Slope One on ml-latest-small dataset")
-cross_validate(algo, data, measures=["rmse", "mae"], cv=10, verbose=True)
+n_fold = 5
+print(f"Cross validation of {n_fold} folds for Slope One on ml-latest-small dataset")
+cross_validate(algo, data, measures=["rmse", "mae"], cv=n_fold, verbose=True)
 
-print("Accuracy of Slope One on ml-latest-small dataset with...")
+print("\nAccuracy of Slope One on ml-latest-small dataset with...")
 print("...75% of dataset used for training, the other 25% used for testing")
 accuracy.rmse(predictions)
 accuracy.mae(predictions)
 
-print("Now, the model uses 100% of dataset to train")
+print("\nNow, the model uses 100% of dataset to train")
 algo.fit(full_trainset)
 full_predictions = algo.test(full_testset)
 store_algo("slopeone_on_ml-latest-small", algo)
 
-print("Prediction for user 1 and movie 6 (the true rating is 4.0)")
 uid, iid = str(1), str(6)
+print(f"\nPrediction for user {uid} and movie {iid} (the true rating is 4.0)")
 algo.predict(uid, iid, r_ui=4, verbose=True)
 
-print("Prediction for user 1 and movie 2 (no true rating exists)")
 uid, iid = str(1), str(2)
+print(f"\nPrediction for user {uid} and movie {iid} (no true rating exists)")
 algo.predict(uid, iid, verbose=True)
 
 n = 50
-print("Prediction for top %d movies of user 1" % n)
+uid = str(1)
+print(f"\nPrediction for top {n} movies of user {uid}")
 top_movies = recommend_top_n(full_predictions, n=n)
 titles_dict = load_movie_titles(dataset_name)
-uid = str(1)
 for movie_id, est_rating in top_movies[uid]:
     print("Rated %.1f - MovieID %s - %s" % (est_rating, movie_id, titles_dict[movie_id]))
