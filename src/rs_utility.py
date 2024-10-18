db4ce60e222a3cf3d83978db847205394cbe36ae
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-10-18 18:10:27 +0700

    fix(utils): revise movie title loading
    the movie title didn't fully loaded due to incorrect way of loading csv

--- src/rs_utility.py
+++ src/rs_utility.py
@@ -1,18 +1,15 @@
-import os
+import os, csv
 from collections import defaultdict
-
 from surprise import Dataset, Reader, dump
-
 
 
 __movielens_reader = Reader(line_format="user item rating timestamp", sep=",", skip_lines=1)
 
 __dataset_path = os.path.relpath("../data/")
-__algo_dump_path = os.path.relpath("../output/algorithms/")
+__algo_dump_path = os.path.relpath("../output/models/")
 
 __ratings_fname = "ratings.csv"
 __movies_fname = "movies.csv"
-
 
 
 def load_movielens(dataset_name):
@@ -20,30 +17,28 @@ def load_movielens(dataset_name):
     return Dataset.load_from_file(file_path, reader=__movielens_reader)
 
 
-
 def store_algo(algo_name, algo):
     file_path = os.path.join(__algo_dump_path, algo_name)
+    dir_path = os.path.dirname(file_path)
+    os.makedirs(dir_path, exist_ok=True)
     dump.dump(file_path, algo=algo)
+
 
 def load_algo(algo_name):
     file_path = os.path.join(__algo_dump_path, algo_name)
     return dump.load(file_path)
 
 
-
 def load_movie_titles(dataset_name):
     movies_path = os.path.join(__dataset_path, dataset_name, __movies_fname)
     movie_dict = defaultdict(list)
     
-    with open(movies_path, encoding="ISO-8859-1") as file:
-        for line in file:
-            fields = line.split(",")
-            movie_id = fields[0]
-            movie_title = fields[1]
-            movie_dict[movie_id] = movie_title
+    with open(movies_path, mode="r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            movie_dict[row["movieId"]] = row["title"]
             
     return movie_dict
-
 
 
 def recommend_top_n(predictions, n=10):
@@ -57,7 +52,6 @@ def recommend_top_n(predictions, n=10):
 		top_n[user] = ratings[:n]
 
 	return top_n
-
 
 
 if __name__ == "__main__":
