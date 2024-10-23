e1a568b75be8b2d951af3b8227b8d99cd2e1a352
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-10-24 00:48:11 +0700

    refactor(knn): apply utils, evals module
    note: knn is decided to be user-based, z-score normalization and use
    pearson similarity according to output of GridSearchCV

---
+++ src/test_knn_zscore.py
@@ -0,0 +1,14 @@
+from utils import load_movielens, load_algo, store_cv
+from evals import cv_kfolds_with_measures
+
+
+algo_name = "knn_zscore_full"
+cv_name = "cv_knn_zscore"
+
+data = load_movielens()
+
+predictions, algo = load_algo(algo_name)
+
+cv_result = cv_kfolds_with_measures(algo, data, n_splits=10, k=100, threshold=3.5)
+
+store_cv(cv_name, cv_result)
---
+++ src/train_knn_zscore.py
@@ -0,0 +1,17 @@
+from surprise import KNNWithZScore
+from utils import load_movielens, store_algo
+
+data = load_movielens()
+trainset = data.build_full_trainset()
+testset = trainset.build_anti_testset()
+
+sim_options = {
+    "name": "pearson",
+    "user_based": True,
+}
+
+algo = KNNWithZScore(k=36, sim_options=sim_options)
+predictions = algo.fit(trainset).test(testset)
+
+algo_dump_name = "knn_zscore_full"
+store_algo(algo_dump_name, predictions, algo)
